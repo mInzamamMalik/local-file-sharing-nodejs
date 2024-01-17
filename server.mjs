a9d88de3cf63a8909df1696cb59e5f4f64961dd8
Inzamam Malik <malikasinger1@gmail.com>
2024-01-17 19:24:09 +0500

    init

---
+++ server.mjs
@@ -0,0 +1,28 @@
+// app.mjs
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+
+// Route to handle file download
+app.get('/download', (req, res) => {
+    const filePath = join(__dirname, 'public', 'largefile.MOV');
+  
+    // Send the file
+    res.sendFile(filePath);
+  });
+
+// Serve static files from the 'public' directory
+app.use(express.static(new URL('public', import.meta.url).pathname));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
